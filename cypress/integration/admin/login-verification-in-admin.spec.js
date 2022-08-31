25b453fddb471f1d0cc5c8e471eca66838c930fc
Vladislav0306 <vlad.ezhkov.96@mail.ru>
2022-08-31 20:48:33 +0300

    Initial commit

---
+++ cypress/integration/admin/login-verification-in-admin.spec.js
@@ -0,0 +1,30 @@
+const login = require('../../fixtures/login.json');
+const store = require('../../fixtures/store.json');
+const selector = require('../../fixtures/selector.json');
+
+describe("When admin is on login page, admin", () => {
+
+    beforeEach(() => {
+        cy.visit("/admin"); // полная ссылка в cypress.json
+    });
+
+    it("Should be able to open the login page", () => {
+        cy.contains(store.adminForEqual).should("be.visible"); // получаем текст из элемента на странице входа
+    })
+
+    it("Should be able to login with correct email and password", () => { // логин с корректными данными
+        cy.login(login.login1, login.password1);
+        cy.contains(store.deskForEqual).should("be.visible");
+    })
+
+    it("Should be able to login with incorrect email and password", () => { // логин с некорректными данными
+        cy.login(login.login2, login.password2);
+        cy.contains(store.mistakeForEqual).should("be.visible");
+    })
+
+    it("Should not be able to login with empty email", () => { // логин с пустым имейлом
+        cy.login(login.login3, login.password3);
+        cy.get(selector.lineNotFilled).then($el => $el[0].checkValidity()).should("be.false"); // get-запрос вернул return (какой-то массив элементов),
+        // у 1го элемента этого массива мы вызываем функцию checkValidity и добавляем assertion. False потому что в консоли элемент возвращает false
+    })
+})
---
+++ cypress/integration/user/home-page-display.spec.js
@@ -0,0 +1,21 @@
+const store = require('../../fixtures/store.json');
+const selector = require('../../fixtures/selector.json');
+
+describe("Should show correct display of the home page", () => {
+
+    beforeEach(() => {
+        cy.visit("/"); // полная ссылка в cypress.json
+    });
+
+    it("Should be able to open the main page", () => { // проверка отображения страницы
+        cy.contains(store.logoHeader); // получаем текст из элемента
+    })
+
+    it("Should show correct number of days", () => {
+        cy.get(selector.week).should("have.length", 7); // проверка что таких селекторов 7 шт (7 дней в неделе)
+    })
+
+    it("Should show correct number of films", () => {
+        cy.get(selector.chooseFilm).should("have.length", 2); // проверка что таких селекторов 2 шт (2 фильма в приложении)
+    })
+})
